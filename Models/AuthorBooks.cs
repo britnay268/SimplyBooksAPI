4eb88df1eaa0a53d9a8757e657ff959fd0e27ed5
Britnay Gore <britnaygore268@gmail.com>
2024-10-04 22:35:00 -0400

    Added required data model for the application - Users, Authors, Books, AuthorBooks

---
+++ Models/AuthorBooks.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace SimplyBooksAPI.Models
+{
+	public class AuthorBooks
+	{
+        public int Id { get; set; }
+
+        public int AuthorId { get; set; }
+        public Authors Author { get; set; }
+
+        public int BookId { get; set; }
+        public Books Book { get; set; }
+
+        public int UserId { get; set; }
+        public Users User { get; set; }
+    }
+}
+

4ff113f8ac9b6fd59f592136d22a38fc84a3a55c
Britnay Gore <britnaygore268@gmail.com>
2024-10-05 11:09:14 -0400

    Installed EF and Postgres

---
+++ Data/SimplyBooksAPIDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SimplyBooksAPI.Data
+{
+	public class SimplyBooksAPIDbContext
+	{
+		public SimplyBooksAPIDbContext()
+		{
+		}
+	}
+}
+
--- obj/Debug/net8.0/SimplyBooksAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/SimplyBooksAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimplyBooksAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eb88df1eaa0a53d9a8757e657ff959fd0e27ed5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4192e8b4e2c2d044cafc7ab1df11bd9535803867")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimplyBooksAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimplyBooksAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

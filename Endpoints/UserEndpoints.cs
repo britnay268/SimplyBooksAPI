4d2bf108dc1c739640ee8d1617be3cfb00ef17f9
Britnay Gore <britnaygore268@gmail.com>
2024-10-07 12:27:19 -0400

    Create endpoint to get all users

---
+++ Endpoints/UserEndpoints.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SimplyBooksAPI.Models;
+using SimplyBooksAPI.Data;
+
+namespace SimplyBooksAPI.Endpoints
+{
+	public static class UserEndpoints
+	{
+        public static void MapUserEndpoints(this IEndpointRouteBuilder routes)
+        {
+            var group = routes.MapGroup("/api/users").WithTags(nameof(Users));
+
+            group.MapGet("/", (SimplyBooksAPIDbContext db) =>
+            {
+                return db.Users;
+            });
+        }
+	}
+}
+
